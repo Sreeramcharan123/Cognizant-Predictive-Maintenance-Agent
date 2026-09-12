42b0733cb79c6b8e249415d24aeecf84a6d5a142
SREERAM CHARAN <sreeramcharan202@gmail.com>
2026-09-12 19:37:19 +0530

    Use environment variable for API_URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ import {
 
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 const LANGUAGE_STORAGE_KEY = "pm_language";
 let ACTIVE_LANGUAGE = "English";
